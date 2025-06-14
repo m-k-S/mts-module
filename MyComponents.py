dd142b7876085036b753611a2978161f03f03475
Iamisen <70295588+Iamisen@users.noreply.github.com>
2025-06-14 17:13:34 -0400

    fixed external components integration and cut out

--- MyComponents.py
+++ MyComponents.py
@@ -1,4 +1,6 @@
 from PyOpticL import optomech
+import math
+inch = 25.4
 
 class BaseComponent:
     '''
@@ -8,16 +10,19 @@ class BaseComponent:
     '''
     type = "Mesh::FeaturePython"
     
-    def __init__(self, obj, stl, color=optomech.adapter_color, **kwargs):
+    def __init__(self, obj, stl, color=optomech.glass_color, thumbscrews=False, drill=True):
+        # required for all object classes
         self.stl = stl
-        drill = kwargs.get('drill', False)
-        thumbscrews = kwargs.get('thumbscrews', False)
 
         obj.Proxy = self
         optomech.ViewProvider(obj.ViewObject)
         obj.ViewObject.ShapeColor = color
         obj.addProperty('App::PropertyBool', 'Drill').Drill = drill
+        obj.addProperty('Part::PropertyPartShape', 'DrillPart')
         obj.addProperty('App::PropertyBool', 'Thumbscrew').Thumbscrew = thumbscrews
+        self.max_angle = 10
+        self.max_width = 1
+
 
     def execute(self, obj):
     #change for other stls
@@ -25,37 +30,46 @@ class BaseComponent:
         mesh.Placement = obj.Mesh.Placement
         obj.Mesh = mesh
 
-class AVC(BaseComponent):
-    def __init__(self, obj, stl=("AVC.stl", (0, 0, 90), (12, 0, 0)), drill=True):
-        super().__init__(obj, stl, drill=drill)
+        part = optomech._bounding_box(obj, 1, 1)
+        part.Placement = obj.Placement
+        obj.DrillPart = part
 
 class KM05(BaseComponent):
-    def __init__(self, obj, stl=("KM05-Step.stl", (90, 0, 90), (2.084, -1.148, 0.498)), drill=True, thumbscrews=True):
-        super().__init__(obj, stl=stl, drill=drill)
+    def __init__(self, obj, stl=("KM05-Step.stl", (90, 0, 90), (2.084, -1.148, 0.498)), color=optomech.adapter_color, drill=True, thumbscrews=True):
+        super().__init__(obj, stl=stl, drill=drill, color=color, thumbscrews=thumbscrews)
 
 class LA1472(BaseComponent):
-    def __init__(self, obj, stl=("LA1472-B-Step.stl", (90, 0, 90), (1.265, 0, 0)), drill=False):
+    def __init__(self, obj, stl=("LA1472-B-Step.stl", (90, 0, 90), (1.265, 0, 0)), drill=True):
         super().__init__(obj, stl, drill=drill)
 
 class LC2067(BaseComponent):
-    def __init__(self, obj, stl=("LC2067-B-Step.stl", (90, 0, 90), (1, 0, 0)), drill=False):
+    def __init__(self, obj, stl=("LC2067-B-Step.stl", (90, 0, 90), (1, 0, 0)), drill=True):
         super().__init__(obj, stl, drill=drill)
 
 class LMR05(BaseComponent):
-    def __init__(self, obj, stl=("LMR05-Step.stl", (90, 0, 90), (1.651, 0, 0)), drill=False):
+    def __init__(self, obj, stl=("LMR05-Step.stl", (90, 0, 90), (1.651, 0, 0)), drill=True):
         super().__init__(obj, stl, drill=drill)
 
 class LMR9(BaseComponent):
-    def __init__(self, obj, stl=("LMR9-Step.stl", (90, 0, 90), (1.651, 0, 0)), drill=False):
+    def __init__(self, obj, stl=("LMR9-Step.stl", (90, 0, 90), (1.651, 0, 0)), drill=True):
         super().__init__(obj, stl, drill=drill)
 
 class Polarizer05(BaseComponent):
-    def __init__(self, obj, stl=("polarizer05.stl", (90, 0, 90), (0, 0, -0)), drill=False):
+    def __init__(self, obj, stl=("polarizer05.stl", (90, 0, 90), (0, 0, -0)), drill=True):
         super().__init__(obj, stl, drill=drill)
+        self.transmission = True
 
 class BeamSplitter(optomech.cube_splitter):
     def __init__(self, obj, *args, **kwargs):
         super().__init__(obj, *args, **kwargs)
+        obj.addProperty('App::PropertyBool', 'Drill').Drill = True
+        obj.addProperty('Part::PropertyPartShape', 'DrillPart')
+
+    def execute(self, obj):
+        super().execute(obj)
+        part = optomech._bounding_box(obj, 3, 4)
+        part.Placement = obj.Placement
+        obj.DrillPart = part
 
 class Photodetector(optomech.photodetector_pda10a2):
     def __init__(self, obj, *args, **kwargs):
@@ -68,3 +82,57 @@ class CircularMirror(optomech.circular_mirror):
 class Fiberport(optomech.fiberport_mount_hca3):
     def __init__(self, obj, *args, **kwargs):
         super().__init__(obj, *args, **kwargs)
+
+
+class AVC():
+    type = 'Part::FeaturePython'
+    def __init__(self, obj, drill=True, thickness=24, diameter=10, part_number='', mount_type=None, mount_args=dict()):
+        obj.Proxy = self
+        optomech.ViewProvider(obj.ViewObject)
+
+        obj.addProperty('App::PropertyBool', 'Drill').Drill = drill
+        obj.addProperty('Part::PropertyPartShape', 'DrillPart')
+        obj.addProperty('App::PropertyLength', 'Thickness').Thickness = thickness
+        obj.addProperty('App::PropertyLength', 'Diameter').Diameter = diameter
+
+        if mount_type != None:
+            optomech._add_linked_object(obj, "Mount", mount_type, pos_offset=(-thickness/2, 0, 0), **mount_args)
+
+        obj.ViewObject.ShapeColor = optomech.glass_color
+        obj.ViewObject.Transparency=50
+        self.part_numbers = [part_number]
+        self.transmission = True
+        self.max_angle = 90
+        self.max_width = diameter
+
+    def execute(self, obj):
+        angle = -math.radians(5)
+        part = optomech._custom_cylinder(
+            dia=obj.Diameter.Value, dz=obj.Thickness.Value,
+            x=-obj.Thickness.Value/2, y=math.sin(angle), z=0,
+            dir=(math.cos(angle), math.sin(angle), 0)
+        )
+        obj.Shape = part
+        part = optomech._bounding_box(obj, 1, 1)
+        part.Placement = obj.Placement
+        obj.DrillPart = part  # <- ADD THIS LINE
+
+        
+
+"""    def execute(self, obj):
+        mesh = optomech._import_stl("AVC_.stl", (0, 0, 90), (12, 0, 0))
+        mesh.Placement = obj.Mesh.Placement
+        obj.Mesh = mesh
+
+        part = optomech._bounding_box(obj, 6, 3)
+        dx = 90
+        for x, y in [(1,1), (-1,1), (1,-1), (-1,-1)]:
+            part = part.fuse(optomech._custom_cylinder(dia=optomech.bolt_8_32['tap_dia'], dz=optomech.drill_depth,
+                                         x=x*dx/2, y=y*15.7, z=-layout.inch/2))
+        part = part.fuse(optomech._custom_cylinder(dia=optomech.bolt_8_32['tap_dia'], dz=optomech.drill_depth,
+                                     x=45, y=-15.7, z=-layout.inch/2))
+        for x in [1,-1]:
+            part = part.fuse(optomech._custom_cylinder(dia=optomech.bolt_8_32['tap_dia'], dz=optomech.drill_depth,
+                                         x=x*dx/2, y=25.7, z=-layout.inch/2))
+        part.Placement = obj.Placement
+        obj.DrillPart = part"""
--- mts.py
+++ mts.py
@@ -10,17 +10,12 @@ name = "MTS"
 date_time = datetime.now().strftime("%m/%d/%Y")
 label = name + " " +  date_time
 
-
-print("Loaded MyComponents from", __file__)
-
-dx,dy,dz = 13,7,1
+dx,dy,dz = 13,7,1.5
 
 # Dimension of the baseplate
 base_dx = dx*layout.inch
 base_dy = dy*layout.inch
 base_dz = dz*layout.inch
-
-
 gap = layout.inch/8
 input_y = (dy-2)*layout.inch
 
@@ -32,7 +27,7 @@ def MTS(x=0, y=0, angle=0, thumbscrews=True):
 	# Difining the baseplate
 	baseplate = layout.baseplate(base_dx, base_dy, base_dz, x=x, y=y, angle=angle, optics_dz = 2.5,
 	                         gap=gap, name=name, label=label, mount_holes=mount_holes)
-	
+
 	# Adding the beams to the baseplate
 	beam = baseplate.add_beam_path(x=gap, y=input_y, angle=layout.cardinal['right'])
 	
@@ -40,36 +35,42 @@ def MTS(x=0, y=0, angle=0, thumbscrews=True):
 	baseplate.place_element("Input Fiberport", optomech.fiberport_mount_hca3, 
 	                    x=gap, y=input_y, angle=layout.cardinal['right'])
 	
-	baseplate.place_element_along_beam("HWP", optomech.waveplate, beam,
-	                               beam_index=0b1, distance=0.5*layout.inch, angle=layout.cardinal['right'],
-								   mount_type=optomech.rotation_stage_rsp05, mount_args=dict(adapter=True, adapter_args=dict(drill=True)))
+	baseplate.place_element_along_beam("Polarizer_1", MyComponents.Polarizer05, beam,
+	                               beam_index=0b1, distance=0.5*layout.inch, angle=layout.cardinal['right'])
 	
-	baseplate.place_element_along_beam("Beam_Splitter", optomech.cube_splitter, beam,
-                                       beam_index=0b1, distance=1.5*layout.inch, angle=layout.cardinal['up'], invert=False)
+	baseplate.place_element_along_beam("Beam_Splitter", MyComponents.BeamSplitter, beam,
+                                       beam_index=0b1, distance=1.5*layout.inch, angle=layout.cardinal['up'])
 	
-	baseplate.place_element_along_beam("Mirror_1", optomech.circular_mirror, beam,
+	baseplate.place_element_along_beam("Vapor_Cell", MyComponents.AVC, beam,
+	                               beam_index=0b10, distance=3*layout.inch, angle=layout.cardinal['left'])
+
+	baseplate.place_element_along_beam("Mirror_1", MyComponents.CircularMirror, beam,
 	                               beam_index=0b11, distance=2.5*layout.inch, angle=layout.turn['down-right'],
-								   mount_type=optomech.mirror_mount_km05, mount_args=dict(drill=True, thumbscrews=True))
+								   mount_type=MyComponents.KM05)
 	
-	# baseplate.place_element_along_beam("EOM", MyComponents.Polarizer05, beam,
-	# 								beam_index=0b11, distance=2*layout.inch, angle=layout.cardinal['right'])
+	baseplate.place_element_along_beam("Polarizer_2", MyComponents.Polarizer05, beam,
+									beam_index=0b11, distance=1*layout.inch, angle=layout.cardinal['left'])
+	
+	baseplate.place_element_along_beam("EOM", MyComponents.Polarizer05, beam,
+									beam_index=0b11, distance=2*layout.inch, angle=layout.cardinal['right'])
 	
 	baseplate.place_element_along_beam("Mirror_2", MyComponents.CircularMirror, beam,
-	                               beam_index=0b11, distance=5*layout.inch, angle=layout.turn['right-up'],
-	                               mount_type=optomech.mirror_mount_km05, mount_args=dict(drill=True, thumbscrews=True))
+	                               beam_index=0b11, distance=3*layout.inch, angle=layout.turn['right-up'],
+	                               mount_type=MyComponents.KM05)
 	
-	# baseplate.place_element_along_beam("Polarizer_3", MyComponents.Polarizer05, beam,
-	# 								beam_index=0b11, distance=1.25*layout.inch, angle=layout.cardinal['down'])
+	baseplate.place_element_along_beam("Polarizer_3", MyComponents.Polarizer05, beam,
+									beam_index=0b11, distance=1.25*layout.inch, angle=layout.cardinal['down'])
 
-	baseplate.place_element_along_beam("Vapor_Cell", MyComponents.AVC, beam,
-	                               beam_index=0b10, distance=2.5*layout.inch, angle=layout.cardinal['right'])
+	#baseplate.place_element_along_beam("Polarizer_3", MyComponents.Polarizer05, beam,
+	#								beam_index=0b10, distance=1*layout.inch, angle=layout.cardinal['left'])
 
-	baseplate.place_element_along_beam("Beam_Splitter_2", optomech.cube_splitter, beam,
-	                               beam_index=0b11, distance=2.5*layout.inch, angle=layout.cardinal['up'], invert=False)
-
-	baseplate.place_element_along_beam("Photodetector", MyComponents.Photodetector, beam,
-	                               beam_index=0b100, distance=2*layout.inch, angle=layout.cardinal['left'])
+	baseplate.place_element_along_beam("Beam_Splitter_2", MyComponents.BeamSplitter, beam,
+	                               beam_index=0b11, distance=1.25*layout.inch, angle=layout.cardinal['down'], invert=True)
+	
+	baseplate.place_element_along_beam("Photodetector", optomech.photodetector_pda10a2, beam,
+	                               beam_index=0b111, distance=2*layout.inch, angle=layout.cardinal['left'])
 
 if __name__ == "__main__":
 	MTS()
+	print("MTS layout created successfully.")
 	layout.redraw()
